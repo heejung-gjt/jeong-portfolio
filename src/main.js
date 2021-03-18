55dbf336284ed047c894d916213aa4ab510b70f7
heejung-gjt <hee_9803@naver.com>
2021-03-18 22:06:59 +0900

    feat: refactoring navigation markup
    - refactoring css style
    - add css responsive

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const nav = document.querySelector('.navigation');
+const ham = document.querySelector('.ir');
+let mql = window.matchMedia('screen and (max-width: 780px)');
+
+// nav 반응형 
+function open_menu_button(){
+    nav.style.transform = `translateX(${0}px)`;
+}
+function close_menu_button(){
+    nav.style.transform = `translateX(${-105}vw)`;
+}
+mql.addListener(function(e){
+    if(e.matches){
+        nav.style.transform = `translateX(${-105}vw)`;
+    }
+    else{
+        nav.style.transform = `translateX(${0}px)`;
+    }
+})
